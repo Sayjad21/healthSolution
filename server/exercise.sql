f33d726827046af91bc682f8e81b0e5ca3859122
sayjad <2105021@cse.buet.ac.bd>
2024-07-12 15:04:40 +0600

    added exercise tables

---
+++ server/exercise.sql
@@ -0,0 +1,26 @@
+CREATE TABLE exercises (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    description TEXT,
+    muscle_groups VARCHAR(255),
+    equipment_needed VARCHAR(100),
+    difficulty_level VARCHAR(50),
+    duration VARCHAR(50),
+    video_url VARCHAR(255),
+    calories_burned DECIMAL(5,2),
+);
+
+
+CREATE TABLE recommendations (
+    id SERIAL PRIMARY KEY,
+    height_range VARCHAR(50) NOT NULL,
+    weight_range VARCHAR(50) NOT NULL,
+    age_range VARCHAR(50) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    bmi_range VARCHAR(50) NOT NULL,
+    exercise_id INT NOT NULL,
+    sets_or_duration VARCHAR(50),
+    FOREIGN KEY (exercise_id) REFERENCES exercises (id) ON DELETE CASCADE
+
+);
